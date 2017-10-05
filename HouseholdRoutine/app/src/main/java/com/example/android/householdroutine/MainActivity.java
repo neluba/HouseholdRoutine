56377a89ea64b8f7647d8905945bf020df0aac11
Oliver Wenzel <oliver.wenzel21@gmail.com>
2017-10-05 17:03:02 +0200

    Initial setup
    • Projekt erstellt
    • Min sdk 16 - Android 4.1 Jelly Bean
    • Menü für die MainActivity schon mal erstellt
    • Farbprofil gesetzt
    • Strings.xml für die Deutscche übersetzung hinzugefügt

---
+++ HouseholdRoutine/app/src/main/java/com/example/android/householdroutine/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.android.householdroutine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    /**
+     * Setting up the menu for the MainActivity
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    /**
+     * On menu item select
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_settings) {
+            // TODO
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
