96937e05b0d9e261d4c9cee083c171fd69aad402
Oliver Wenzel <oliver.wenzel21@gmail.com>
2017-10-18 02:37:51 +0200

    Zusatz zu teil 2
    Vergessen das mit zu schicken

--- HouseholdRoutine/app/src/main/java/com/example/android/householdroutine/CreateTask.java
+++ HouseholdRoutine/app/src/main/java/com/example/android/householdroutine/CreateTask.java
@@ -2,18 +2,29 @@ package com.example.android.householdroutine;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class CreateTask extends AppCompatActivity {
 
@@ -21,20 +32,38 @@ public class CreateTask extends AppCompatActivity {
     private EditText description;
     private EditText endDate;
     private EditText endTime;
-    private Calendar calendar = Calendar.getInstance();
+    private RadioButton reminder;
+    private RadioButton checklist;
+    private ConstraintLayout checklistView;
+    private RadioButton checklistTypePredefined;
+    private RadioButton checklistTypeOwn;
+    private LinearLayout ownChecklistView;
+    private EditText ownChecklistAddItemName;
 
+    private Calendar calendar = Calendar.getInstance();
+    private List<String> checklistItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
 
+        checklistItems = new ArrayList<String>();
+
         name = (EditText) findViewById(R.id.new_task_name);
         description = (EditText) findViewById(R.id.new_task_description);
+        endDate = (EditText) findViewById(R.id.new_task_end_date);
+        endTime = (EditText) findViewById(R.id.new_task_end_time);
+        reminder = (RadioButton) findViewById(R.id.new_task_type_button1);
+        checklist = (RadioButton) findViewById(R.id.new_task_type_button2);
+        checklistView = (ConstraintLayout) findViewById(R.id.new_task_checklist_view);
+        checklistTypePredefined = (RadioButton) findViewById(R.id.new_task_checklist_type_button1);
+        checklistTypeOwn = (RadioButton) findViewById(R.id.new_task_checklist_type_button2);
+        ownChecklistView = (LinearLayout) findViewById(R.id.new_task_checklist_type_own);
+        ownChecklistAddItemName = (EditText) findViewById(R.id.own_checklist_add_item_name);
 
         // end date date picker
         // source: https://stackoverflow.com/questions/14933330/datepicker-how-to-popup-datepicker-when-click-on-edittext
-        endDate = (EditText) findViewById(R.id.new_task_end_date);
         endDate.setKeyListener(null);
         final DatePickerDialog.OnDateSetListener dateListener = new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -60,7 +89,6 @@ public class CreateTask extends AppCompatActivity {
         });
 
         // end time time picker
-        endTime = (EditText) findViewById(R.id.new_task_end_time);
         endTime.setKeyListener(null);
         final TimePickerDialog.OnTimeSetListener timeListener = new TimePickerDialog.OnTimeSetListener() {
             @Override
@@ -84,18 +112,59 @@ public class CreateTask extends AppCompatActivity {
             }
         });
 
-
-        // fab
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.new_task_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // task type radio buttons
+        reminder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Todo methode schreiben, mit der die aufgabe erstellt wird und es zurück zur main activity geht. Wenn ein feld nicht ausgefüllt wurde, kommt ein neuer Toast
-                Toast.makeText(CreateTask.this, "Not ready yet", Toast.LENGTH_SHORT).show();
+                checklistView.setVisibility(View.INVISIBLE);
+            }
+        });
+        checklist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checklistView.setVisibility(View.VISIBLE);
             }
         });
 
 
+        // checklist type radio buttons
+        checklistTypePredefined.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ownChecklistView.setVisibility(View.INVISIBLE);
+                // predefined checklist VISIBLE
+            }
+        });
+        checklistTypeOwn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ownChecklistView.setVisibility(View.VISIBLE);
+                // predefined cheecklist INVISIBLE
+            }
+        });
+
+
+    }
+
+
+    // create menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.create_task, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.save_task) {
+            // Todo methode schreiben, mit der die aufgabe erstellt wird und es zurück zur main activity geht. Wenn ein feld nicht ausgefüllt wurde, kommt ein neuer Toast
+            Toast.makeText(CreateTask.this, "Not ready yet", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -116,4 +185,44 @@ public class CreateTask extends AppCompatActivity {
         endTime.setText(dateFormat.format(date));
     }
 
+    /**
+     * Adds own items to the linear layout for the checklist items
+     * @param v
+     */
+    public void addItem(View v) {
+        String name = ownChecklistAddItemName.getText().toString();
+        if(!TextUtils.isEmpty(name) && name != null) {
+            LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            final View checklistItemView = inflater.inflate(R.layout.create_task_checklist_item, null);
+
+            TextView itemTextView = (TextView) checklistItemView.findViewById(R.id.own_checklist_item_name);
+            itemTextView.setText(name);
+            ownChecklistView.addView(checklistItemView);
+
+            checklistItems.add(name);
+
+            ownChecklistAddItemName.setText("");
+        }
+    }
+
+    /**
+     * Removes an item from the linear view for checklist items
+     * @param v
+     */
+    public void removeItem(View v) {
+        View parent = (View) v.getParent();
+        TextView itemTextView = (TextView) parent.findViewById(R.id.own_checklist_item_name);
+        String name = itemTextView.getText().toString();
+
+        for(int i = 0; i < checklistItems.size(); i++) {
+            if(checklistItems.get(i).equals(name)) {
+                checklistItems.remove(i);
+                break;
+            }
+        }
+
+        ownChecklistView.removeView((View) v.getParent());
+
+
+    }
 }
